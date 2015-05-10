e411d8d65c46a2bf50b30c49174135f8aabfa13b
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-05-09 23:57:52 -0300

    Update Problem06.c

--- Problem06.c
+++ Problem06.c
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
 	int sum2;
 	sum1 = pow(((((1 + 100)*100)/2)),2); //using arithmetic progression sum formula
 	sum2 = (100*(100+1)*(2*100+1)/6); //knowing that the sum of the n first square integers
-									//is given(n(n+1)(2n+1))/6
+					//is given by (n(n+1)(2n+1))/6
 	
 	printf("sum1 = %d and sum2 = %d --- %d - %d=%d",sum1,sum2,sum1,sum2,sum1-sum2);
 	return 0;
