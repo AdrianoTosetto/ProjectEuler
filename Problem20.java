99899579e2f7b3754a187ff1ec7f0c3903db6527
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-05-13 01:31:00 -0300

    Create Problem20.java

---
+++ Problem20.java
@@ -0,0 +1,32 @@
+
+import java.math.BigDecimal;
+
+public class Problem20 {
+
+		
+	//Solution in Java for Project Euler number 20 problem
+	public BigDecimal factorial(int n){
+		BigDecimal fat = new BigDecimal("1");
+		for(int i = 2; i < n;i++){
+			fat = fat.multiply(new BigDecimal(i));
+		}
+		
+		return fat;
+	}
+	
+	
+	public int sumDigits(String strNumber){
+		int sum = 0;
+		for(int i = 0; i < strNumber.length();i++){
+			sum+= Integer.parseInt(Character.toString(strNumber.charAt(i)));
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+
+		int sum = new Problem20().sumDigits(new Problem20().factorial(100).toString());
+		System.out.println("The sum is " + sum);
+	}
+
+}
